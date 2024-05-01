ce0f166e07624d2aa4725043a429c73f18e941fb
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2024-04-30 20:50:33 -0700

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,27 +7,27 @@
  */
 void selection_sort(int *array, size_t size)
 {
-        int temp;
-        size_t i, j, min;
+	int temp;
+	size_t i, j, min;
 
-        if (array == NULL || size < 2)
-                return;
+	if (array == NULL || size < 2)
+		return;
 
-        for (i = 0; i < size - 1; i++)
-        {
-                min = i;
-                for (j = i + 1; j < size; j++)
-                {
-                        if (array[j] < array[min])
-                                min = j;
-                }
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
 
-                if (min != i)
-                {
-                        temp = array[i];
-                        array[i] = array[min];
-                        array[min] = temp;
-                        print_array(array, size);
-                }
-        }
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
 }
